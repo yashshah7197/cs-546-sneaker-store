f59d7967841461e78fb4ad8baea69d12a45ace90
Hamza <hamzasbuch@gmail.com>
2021-11-27 18:35:30 -0500

    Q and A's DB and Review rating calculation changes

--- routes/sneakers.js
+++ routes/sneakers.js
@@ -1,62 +1,64 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const data = require('../data');
+const data = require("../data");
 const sneakersData = data.sneakers;
 
-const {ObjectId} = require('mongodb');
+const { ObjectId } = require("mongodb");
 
-router.get('/listedBy/:id', async(req, res) => 
-{
-    try {
-        
-        const sneakers = await sneakersData.getAllListedBy(req.params.id);
-        res.render('store/sneakerListedby',{sneakers:sneakers});
-      } catch (e) {
-        res.sendStatus(500);
-      }
+router.get("/listedBy/:id", async (req, res) => {
+  try {
+    const sneakers = await sneakersData.getAllListedBy(req.params.id);
+    res.render("store/sneakerListedby", { sneakers: sneakers });
+  } catch (e) {
+    res.sendStatus(500);
+  }
 });
 
-router.get('/', async(req, res) => 
-{
-    try {
-        const sneakers = await sneakersData.getAll();
-        res.render('store/sneakersList',{sneakers:sneakers});
-      } catch (e) {
-        res.sendStatus(500);
-      }
+router.get("/", async (req, res) => {
+  try {
+    const sneakers = await sneakersData.getAll();
+    res.render("store/sneakersList", { sneakers: sneakers });
+  } catch (e) {
+    res.sendStatus(500);
+  }
 });
-router.get('/:id', async(req, res) => 
-{
-    try {
-        const sneaker = await sneakersData.get(req.params.id);
-        res.render('store/sneakerBuy',{sneaker:sneaker});
-      } catch (e) {
-        res.status(404).json({ message: ' There is no Sneaker with that ID' });
-      }
+router.get("/:id", async (req, res) => {
+  try {
+    const sneaker = await sneakersData.get(req.params.id);
+    res.render("store/sneakerBuy", { sneaker: sneaker });
+  } catch (e) {
+    res.status(404).json({ message: " There is no Sneaker with that ID" });
+  }
 });
-router.get('/listedByUpdate/:id', async(req, res) => 
-{
-    try {
-        console.log(req.params.id);
-        const sneaker = await sneakersData.get(req.params.id);
-        console.log(sneaker);
+router.get("/listedByUpdate/:id", async (req, res) => {
+  try {
+    console.log(req.params.id);
+    const sneaker = await sneakersData.get(req.params.id);
+    console.log(sneaker);
 
-        res.render('store/sneakerUpdate',{sneaker:sneaker});
-        console.log("hell2");
-
-      } catch (e) {
-        res.status(404).json({ message: ' There is no Sneaker with that ID' });
-      }
+    res.render("store/sneakerUpdate", { sneaker: sneaker });
+    console.log("hell2");
+  } catch (e) {
+    res.status(404).json({ message: " There is no Sneaker with that ID" });
+  }
 });
-router.get('/delete/:id', async(req, res) => 
-{
-   
-try {
+router.get("/delete/:id", async (req, res) => {
+  try {
     const sneaker = await sneakersData.remove(req.params.id);
     res.redirect("/sneakers/");
   } catch (e) {
-      console.log(e);
-    res.status(404).json({ message: 'There is no Restaurant with that ID' });
+    console.log(e);
+    res.status(404).json({ message: "There is no Restaurant with that ID" });
+  }
+});
+
+//Route for individual sneaker page
+router.get("/sneaker/:id", async (req, res) => {
+  try {
+    //const sneakers = await sneakersData.get(req.params.id);
+    res.render("store/sneakerInfo", { title: "Individual Sneaker page" });
+  } catch (e) {
+    res.sendStatus(500);
   }
 });
 module.exports = router;
