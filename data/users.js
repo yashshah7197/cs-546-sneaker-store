8af8de7312c56da6cd78ad19aeebbef6e4293274
Hamza <hamzasbuch@gmail.com>
2021-11-30 16:18:19 -0500

    Accordion bug fix

--- data/users.js
+++ data/users.js
@@ -1,175 +1,196 @@
-const mongoCollections = require('../config/mongoCollections');
+const mongoCollections = require("../config/mongoCollections");
 const users = mongoCollections.users;
 
-const {ObjectId} = require('mongodb');
-const {checkInputStr} = require("./validate");
-const bcrypt = require("bcrypt");
+const { ObjectId } = require("mongodb");
+const { checkInputStr } = require("./validate");
+const bcrypt = require("bcryptjs");
 
-const create = async (firstName, lastName, email, password, address, phoneNumber, isAdmin, sneakersListed, sneakersBought) => {
-    checkInputStr(firstName);
-    checkInputStr(lastName);
-    checkInputStr(email);
-    checkInputStr(password);
-    checkInputStr(address);
-    checkInputStr(phoneNumber);
+const create = async (
+  firstName,
+  lastName,
+  email,
+  password,
+  address,
+  phoneNumber,
+  isAdmin,
+  sneakersListed,
+  sneakersBought
+) => {
+  checkInputStr(firstName);
+  checkInputStr(lastName);
+  checkInputStr(email);
+  checkInputStr(password);
+  checkInputStr(address);
+  checkInputStr(phoneNumber);
 
-    const usersCollection = await users();
+  const usersCollection = await users();
 
-    let user = usersCollection.findOne({
-        email: email.toLowerCase()
-    });
+  let user = usersCollection.findOne({
+    email: email.toLowerCase(),
+  });
 
-    if (user) {
-        throw {
-            statusCode: 400,
-            message: "A user with email " + email + " already exists in the system!"
-        }
-    }
-
-    let newUser = {
-        _id: ObjectId(),
-        email: email,
-        passwordHash: await hashPassword(password),
-        firstName: firstName,
-        lastName: lastName,
-        address: address,
-        phoneNumber: phoneNumber,
-        isAdmin: isAdmin,
-        sneakersListed: [],
-        sneakersBought: []
+  if (user) {
+    throw {
+      statusCode: 400,
+      message: "A user with email " + email + " already exists in the system!",
     };
+  }
 
-    const insertInfo = await usersCollection.insertOne(newUser);
-    if (insertInfo.insertedCount === 0) {
-        throw {
-            statusCode: 500,
-            message: "Internal server error!"
-        }
-    }
+  let newUser = {
+    _id: ObjectId(),
+    email: email,
+    passwordHash: await hashPassword(password),
+    firstName: firstName,
+    lastName: lastName,
+    address: address,
+    phoneNumber: phoneNumber,
+    isAdmin: isAdmin,
+    sneakersListed: [],
+    sneakersBought: [],
+  };
 
-    return {userInserted: true};
+  const insertInfo = await usersCollection.insertOne(newUser);
+  if (insertInfo.insertedCount === 0) {
+    throw {
+      statusCode: 500,
+      message: "Internal server error!",
+    };
+  }
+
+  return { userInserted: true };
 };
 
 const getAll = async () => {
-    const usersCollection = await users();
+  const usersCollection = await users();
 
-    const usersArray = await usersCollection.find().toArray();
-    usersArray.forEach((user) => {
-        user["passwordHash"] = "";
-    });
+  const usersArray = await usersCollection.find().toArray();
+  usersArray.forEach((user) => {
+    user["passwordHash"] = "";
+  });
 
-    return usersArray;
+  return usersArray;
 };
 
 const get = async (userId) => {
-    checkInputStr(userId);
+  checkInputStr(userId);
 
-    try {
-        userId = ObjectId(userId.trim());
-    } catch (e) {
-        throw {
-            statusCode: 400,
-            message: "Could not parse the id in to a valid ObjectId!"
-        }
-    }
+  try {
+    userId = ObjectId(userId.trim());
+  } catch (e) {
+    throw {
+      statusCode: 400,
+      message: "Could not parse the id in to a valid ObjectId!",
+    };
+  }
 
-    const usersCollection = await users();
+  const usersCollection = await users();
 
-    const user = await usersCollection.findOne({_id: userId});
-    if (user === null) {
-        throw {
-            statusCode: 404,
-            message: "No user was found with the given id!"
-        }
-    }
+  const user = await usersCollection.findOne({ _id: userId });
+  if (user === null) {
+    throw {
+      statusCode: 404,
+      message: "No user was found with the given id!",
+    };
+  }
 
-    user["passwordHash"] = "";
+  user["passwordHash"] = "";
 
-    return user;
+  return user;
 };
 
-const update = async (userId, firstName, lastName, email, password, address, phoneNumber, isAdmin, sneakersListed, sneakersBought) => {
-    checkInputStr(userId);
-    checkInputStr(firstName);
-    checkInputStr(lastName);
-    checkInputStr(email);
-    checkInputStr(password);
-    checkInputStr(address);
-    checkInputStr(phoneNumber);
+const update = async (
+  userId,
+  firstName,
+  lastName,
+  email,
+  password,
+  address,
+  phoneNumber,
+  isAdmin,
+  sneakersListed,
+  sneakersBought
+) => {
+  checkInputStr(userId);
+  checkInputStr(firstName);
+  checkInputStr(lastName);
+  checkInputStr(email);
+  checkInputStr(password);
+  checkInputStr(address);
+  checkInputStr(phoneNumber);
 
-    try {
-        userId = ObjectId(userId.trim());
-    } catch (e) {
-        throw {
-            statusCode: 400,
-            message: "Could not parse the user id in to a valid ObjectId!"
-        }
-    }
-
-    const user = await get(userId.toString());
-    const updatedUser = {
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        password: await hashPassword(password),
-        address: address,
-        phoneNumber: phoneNumber,
-        isAdmin: isAdmin,
-        sneakersListed: sneakersListed,
-        sneakersBought: sneakersBought
+  try {
+    userId = ObjectId(userId.trim());
+  } catch (e) {
+    throw {
+      statusCode: 400,
+      message: "Could not parse the user id in to a valid ObjectId!",
     };
+  }
 
-    const usersCollection = await users();
+  const user = await get(userId.toString());
+  const updatedUser = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    password: await hashPassword(password),
+    address: address,
+    phoneNumber: phoneNumber,
+    isAdmin: isAdmin,
+    sneakersListed: sneakersListed,
+    sneakersBought: sneakersBought,
+  };
 
-    const updatedInfo = await usersCollection.updateOne(
-        {_id: userId},
-        {$set: updatedUser}
-    );
+  const usersCollection = await users();
 
-    if (!updatedInfo.matchedCount && !updatedInfo.modifiedCount) {
-        throw {
-            statusCode: 500,
-            message: "Internal server error!"
-        }
-    }
+  const updatedInfo = await usersCollection.updateOne(
+    { _id: userId },
+    { $set: updatedUser }
+  );
 
-    return await get(userId.toString());
+  if (!updatedInfo.matchedCount && !updatedInfo.modifiedCount) {
+    throw {
+      statusCode: 500,
+      message: "Internal server error!",
+    };
+  }
+
+  return await get(userId.toString());
 };
 
 const remove = async (userId) => {
-    checkInputStr(userId);
+  checkInputStr(userId);
 
-    try {
-        userId = ObjectId(userId.trim());
-    } catch (e) {
-        throw {
-            statusCode: 400,
-            message: "Could not parse the user id in to a valid ObjectId!"
-        }
-    }
+  try {
+    userId = ObjectId(userId.trim());
+  } catch (e) {
+    throw {
+      statusCode: 400,
+      message: "Could not parse the user id in to a valid ObjectId!",
+    };
+  }
 
-    const user = await get(userId.toString());
+  const user = await get(userId.toString());
 
-    const usersCollection = await users();
+  const usersCollection = await users();
 
-    const deletionInfo = await usersCollection.deleteOne({_id: userId});
-    if (deletionInfo.deletedCount === 0) {
-        throw {
-            statusCode: 500,
-            message: "Internal server error!"
-        }
-    }
+  const deletionInfo = await usersCollection.deleteOne({ _id: userId });
+  if (deletionInfo.deletedCount === 0) {
+    throw {
+      statusCode: 500,
+      message: "Internal server error!",
+    };
+  }
 };
 
 const hashPassword = async (password) => {
-    const saltRounds = 16;
-    return await bcrypt.hash(password, saltRounds);
-}
+  const saltRounds = 16;
+  return await bcrypt.hash(password, saltRounds);
+};
 
 module.exports = {
-    create,
-    getAll,
-    get,
-    update,
-    remove
+  create,
+  getAll,
+  get,
+  update,
+  remove,
 };
