59e72e9da922bd85f7cf5a67e4955594bc9f8e1d
Hamza <hamzasbuch@gmail.com>
2021-12-09 18:47:07 -0500

    Buy error handling

--- public/js/client.js
+++ public/js/client.js
@@ -1,26 +1,26 @@
 //Commented by Hamza || Code to be modified
 
-// (function ($) {
-//   $(document).ready(function () {
-//     $(".alert").alert("close");
-//     $("select").change(function () {
-//       $(".alert").alert("close");
-//       var nameArr = $("#size").val().split(",");
-//       console.log(nameArr[0]);
-//       if ($("#size").val() == 0) {
-//         document.getElementById("notify").hidden = true;
-//         $("#buy").attr("disabled", true);
-//       } else if (nameArr[1] <= 0) {
-//         document.getElementById("notify").hidden = false;
-//         $("#buy").attr("disabled", true);
-//       } else {
-//         document.getElementById("notify").hidden = true;
-//         $("#buy").attr("disabled", false);
-//       }
-//     });
-//   });
+(function ($) {
+  $(document).ready(function () {
+    $(".alert").alert("close");
+    $("select").change(function () {
+      $(".alert").alert("close");
+      var nameArr = $("#size").val().split(",");
+      console.log(nameArr[0]);
+      if ($("#size").val() == 0) {
+        document.getElementById("notify").hidden = true;
+        $("#buy").attr("disabled", true);
+      } else if (nameArr[1] <= 0) {
+        document.getElementById("notify").hidden = false;
+        $("#buy").attr("disabled", true);
+      } else {
+        document.getElementById("notify").hidden = true;
+        $("#buy").attr("disabled", false);
+      }
+    });
+  });
 
-//   $("#notify").click(function (event) {
-//     $(".alert").alert();
-//   });
-// })(window.jQuery);
+  $("#notify").click(function (event) {
+    $(".alert").alert();
+  });
+})(window.jQuery);
