365ec0f68340b42ad0371323c9415bf6668007d7
Hamza <hamzasbuch@gmail.com>
2021-12-09 17:46:23 -0500

    QandA's validation check addition

--- public/js/client.js
+++ public/js/client.js
@@ -1,24 +1,26 @@
-(function ($) {
-  $(document).ready(function () {
-    $(".alert").alert("close");
-    $("select").change(function () {
-      $(".alert").alert("close");
-      var nameArr = $("#size").val().split(",");
-      console.log(nameArr[0]);
-      if ($("#size").val() == 0) {
-        document.getElementById("notify").hidden = true;
-        $("#buy").attr("disabled", true);
-      } else if (nameArr[1] <= 0) {
-        document.getElementById("notify").hidden = false;
-        $("#buy").attr("disabled", true);
-      } else {
-        document.getElementById("notify").hidden = true;
-        $("#buy").attr("disabled", false);
-      }
-    });
-  });
+//Commented by Hamza || Code to be modified
 
-  $("#notify").click(function (event) {
-    $(".alert").alert();
-  });
-})(window.jQuery);
+// (function ($) {
+//   $(document).ready(function () {
+//     $(".alert").alert("close");
+//     $("select").change(function () {
+//       $(".alert").alert("close");
+//       var nameArr = $("#size").val().split(",");
+//       console.log(nameArr[0]);
+//       if ($("#size").val() == 0) {
+//         document.getElementById("notify").hidden = true;
+//         $("#buy").attr("disabled", true);
+//       } else if (nameArr[1] <= 0) {
+//         document.getElementById("notify").hidden = false;
+//         $("#buy").attr("disabled", true);
+//       } else {
+//         document.getElementById("notify").hidden = true;
+//         $("#buy").attr("disabled", false);
+//       }
+//     });
+//   });
+
+//   $("#notify").click(function (event) {
+//     $(".alert").alert();
+//   });
+// })(window.jQuery);
--- public/js/shop.js
+++ public/js/shop.js
@@ -1,5 +1,5 @@
 (function ($) {
-  var answerForm = $(".answerForm");
+  //var answerForm = $(".answerForm");
   var questionForm = $("#questionForm");
   var reviewForm = $("#reviewForm");
   var reviewFormError = $("#reviewFormError");
@@ -9,7 +9,6 @@
   //Add review AJAX call
   reviewForm.submit(function (event) {
     event.preventDefault();
-    debugger;
     try {
       reviewFormError.addClass("d-none");
       var reviewedByElem = $("#reviewedBy");
@@ -21,7 +20,7 @@
       checkInputStr(reviewedByElem.val(), "reviewedBy");
       checkInputStr(reviewForElem.val(), "reviewFor");
       checkInputStr(reviewTitleElem.val(), "Title");
-      checkInputStr(reviewTextElem.val(), "Text");
+      checkInputStr(reviewTextElem.val(), "Review");
       checkIsNumber(Number(reviewRatingElem.val()), "Rating");
       checkRating(Number(reviewRatingElem.val()));
 
@@ -72,8 +71,9 @@
           $("#reviewText").val("");
           $("#reviewRating").val("");
         })
-        .catch(function (e) {
-          reviewErrorMsg.empty().append(e);
+        .fail(function (e) {
+          errorMessage = JSON.parse(e.responseText);
+          reviewErrorMsg.empty().append(errorMessage.Error.message);
           reviewFormError.removeClass("d-none");
         });
     } catch (e) {
@@ -86,8 +86,8 @@
   //Ask Question AJAX call
   questionForm.submit(function (event) {
     event.preventDefault();
-    debugger;
     try {
+      quesFormError.addClass("d-none");
       var values = {};
 
       var x = $("#questionForm").serializeArray();
@@ -95,79 +95,93 @@
         values[field.name] = field.value;
       });
 
+      var qAndAForId = values["qAndAFor"];
+      var questionById = values["questionBy"];
+      var questionText = values["question"];
+
+      checkInputStr(qAndAForId, "QandA ID");
+      checkInputStr(questionById, "Question By");
+      checkInputStr(questionText, "Question");
+
       var requestConfig = {
         method: "POST",
         url: "/qAndA/",
         dataType: "json",
         data: {
-          qAndAFor: values["qAndAFor"],
-          questionBy: values["questionBy"],
-          question: values["question"],
+          qAndAFor: qAndAForId,
+          questionBy: questionById,
+          question: questionText,
         },
       };
 
-      $.ajax(requestConfig).then(function (responseMessage) {
-        // var newHTML = `<div class="accordion-item">
-        //   <h4 class="accordion-header" id="heading_${responseMessage._id}">
-        //     <button
-        //       class="accordion-button"
-        //       type="button"
-        //       data-bs-toggle="collapse"
-        //       data-bs-target="#collapse_${responseMessage._id}"
-        //       aria-expanded="true"
-        //       aria-controls="collapse_${responseMessage._id}"
-        //     >
-        //     ${responseMessage.question} - User : ${responseMessage.questionBy}
-        //     </button>
-        //   </h4>
-        //   <div
-        //     id="collapse_${responseMessage._id}"
-        //     class="accordion-collapse collapse"
-        //     aria-labelledby="heading_${responseMessage._id}"
-        //     data-bs-parent="#accordionQ"
-        //   >
-        //     <div class="accordion-body" id="answerBody_${responseMessage._id}">
-        //       <h4>Answers</h4>
-        //       <div id="answerPanel_${responseMessage._id}">
-        //           <hr />
-        //           <div>No answers</div>
-        //           <hr />
-        //       </div>
+      $.ajax(requestConfig)
+        .then(function (responseMessage) {
+          // var newHTML = `<div class="accordion-item">
+          //   <h4 class="accordion-header" id="heading_${responseMessage._id}">
+          //     <button
+          //       class="accordion-button"
+          //       type="button"
+          //       data-bs-toggle="collapse"
+          //       data-bs-target="#collapse_${responseMessage._id}"
+          //       aria-expanded="true"
+          //       aria-controls="collapse_${responseMessage._id}"
+          //     >
+          //     ${responseMessage.question} - User : ${responseMessage.questionBy}
+          //     </button>
+          //   </h4>
+          //   <div
+          //     id="collapse_${responseMessage._id}"
+          //     class="accordion-collapse collapse"
+          //     aria-labelledby="heading_${responseMessage._id}"
+          //     data-bs-parent="#accordionQ"
+          //   >
+          //     <div class="accordion-body" id="answerBody_${responseMessage._id}">
+          //       <h4>Answers</h4>
+          //       <div id="answerPanel_${responseMessage._id}">
+          //           <hr />
+          //           <div>No answers</div>
+          //           <hr />
+          //       </div>
 
-        //       <form class="answerForm" id="answerForm_${responseMessage._id}" method="POST" onclick="submitAnswer(event)">
-        //         <div class="mb-3">
-        //           <label for="answerText_${responseMessage._id}" class="form-label">Answer this
-        //             question</label>
-        //           <input
-        //             type="text"
-        //             name="answerText"
-        //             class="form-control"
-        //             id="answerText_${responseMessage._id}"
-        //           />
-        //           <input
-        //             type="hidden"
-        //             value="${responseMessage.questionBy}"
-        //             name="answerBy"
-        //             id="answerBy_${responseMessage._id}"
-        //           />
-        //           <input
-        //             type="hidden"
-        //             name="_method"
-        //             value="put"
-        //             id="method_${responseMessage._id}"
-        //           />
-        //           <input type="submit" value="Submit" />
-        //         </div>
-        //       </form>
-        //     </div>
-        //   </div>
-        // </div>`;
-        // $("#accordionQ").append(newHTML);
-        // $("#collapsePostQ").removeClass("show");
-        // $("#question").val("");
+          //       <form class="answerForm" id="answerForm_${responseMessage._id}" method="POST" onclick="submitAnswer(event)">
+          //         <div class="mb-3">
+          //           <label for="answerText_${responseMessage._id}" class="form-label">Answer this
+          //             question</label>
+          //           <input
+          //             type="text"
+          //             name="answerText"
+          //             class="form-control"
+          //             id="answerText_${responseMessage._id}"
+          //           />
+          //           <input
+          //             type="hidden"
+          //             value="${responseMessage.questionBy}"
+          //             name="answerBy"
+          //             id="answerBy_${responseMessage._id}"
+          //           />
+          //           <input
+          //             type="hidden"
+          //             name="_method"
+          //             value="put"
+          //             id="method_${responseMessage._id}"
+          //           />
+          //           <input type="submit" value="Submit" />
+          //         </div>
+          //       </form>
+          //     </div>
+          //   </div>
+          // </div>`;
+          // $("#accordionQ").append(newHTML);
+          // $("#collapsePostQ").removeClass("show");
+          // $("#question").val("");
 
-        window.location.reload();
-      });
+          window.location.reload();
+        })
+        .fail(function (e) {
+          errorMessage = JSON.parse(e.responseText);
+          quesErrorMsg.empty().append(errorMessage.Error.message);
+          quesFormError.removeClass("d-none");
+        });
     } catch (e) {
       //console.log(e);
       quesErrorMsg.empty().append(e);
@@ -222,48 +236,67 @@
 //Answer AJAX call
 function submitAnswer(event) {
   event.preventDefault();
+  var ansFormError;
+  var ansErrorMsg;
   try {
     var qID = event.currentTarget.id;
     qID = qID.replace("answerForm_", "");
+
+    var ansFormError = $("#ansFormError_" + qID);
+    var ansErrorMsg = $("#ansErrorMsg_" + qID);
+
+    ansFormError.addClass("d-none");
+
+    var answerByID = $("#answerBy_" + qID).val();
+    var answerText = $("#answerText_" + qID).val();
+
+    checkInputStr(answerByID, "Answer By");
+    checkInputStr(answerText, "Answer");
+
     var requestConfig = {
       method: $("#method_" + qID).val(),
       url: "/qAndA/" + qID,
       dataType: "json",
       data: {
-        answerBy: $("#answerBy_" + qID).val(),
-        //answer: values["answerText"],
-        answer: $("#answerText_" + qID).val(),
+        answerBy: answerByID,
+        answer: answerText,
       },
     };
 
-    $.ajax(requestConfig).then(function (responseMessage) {
-      if ($(`#answerPanel_${responseMessage._id} .answerBlock`).length > 0) {
-        var newHTML = `<hr />
+    $.ajax(requestConfig)
+      .then(function (responseMessage) {
+        if ($(`#answerPanel_${responseMessage._id} .answerBlock`).length > 0) {
+          var newHTML = `<hr />
                   <div>
                     <div>Answer: ${responseMessage.answer.answer}</div>
                     <div>User: ${responseMessage.answer.answeredBy}</div>
                   </div>
                   <hr />`;
-        $(`#answerPanel_${responseMessage._id}`).append(newHTML);
-      } else {
-        var newHTML = `<hr />
+          $(`#answerPanel_${responseMessage._id}`).append(newHTML);
+        } else {
+          var newHTML = `<hr />
                   <div>
                     <div>Answer: ${responseMessage.answer.answer}</div>
                     <div>User: ${responseMessage.answer.answeredBy}</div>
                   </div>
                   <hr />`;
-        $(`#answerPanel_${responseMessage._id}`).empty().append(newHTML);
-      }
+          $(`#answerPanel_${responseMessage._id}`).empty().append(newHTML);
+        }
 
-      $("#answerText_" + qID).val("");
-    });
+        $("#answerText_" + qID).val("");
+      })
+      .fail(function (e) {
+        errorMessage = JSON.parse(e.responseText);
+        ansErrorMsg.empty().append(errorMessage.Error.message);
+        ansFormError.removeClass("d-none");
+      });
   } catch (e) {
-    console.log(e);
+    ansErrorMsg.empty().append(e);
+    ansFormError.removeClass("d-none");
   }
 }
 
-function reportReview(e, userID) {
-
-  console.log("Review reported Successfully");
-  console.log(userID);
-}
+// function reportReview(event) {
+//   var repoertedBy = $("#reviewedBy").val();
+//   console.log("Review reported Successfully");
+// }
