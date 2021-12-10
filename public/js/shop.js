5ecccc91e71e57d65ba3f9e42045323063fe2695
Hamza <hamzasbuch@gmail.com>
2021-12-10 14:08:30 -0500

    Buy page client side validations

--- public/js/shop.js
+++ public/js/shop.js
@@ -3,19 +3,17 @@
   var questionForm = $("#questionForm");
   var reviewForm = $("#reviewForm");
   var reviewFormError = $("#reviewFormError");
-  var reviewErrorMsg = $("#reviewErrorMsg");
   var quesFormError = $("#quesFormError");
-  var quesErrorMsg = $("#quesErrorMsg");
 
   var sneakerBuyForm = $("#sneakerBuyForm");
 
   //Sneaker buy client side validations
   sneakerBuyForm.submit(function (event) {
     var buyFormError;
-    var buyErrorMsg;
     try {
       var buyFormError = $("#buyFormError");
-      var buyErrorMsg = $("#buyErrorMsg");
+
+      buyFormError.addClass("d-none");
 
       var sneakerId = $("#sneakerId").val();
       var sneakerPrice = $("#sneakerPrice").val();
@@ -29,13 +27,13 @@
       checkIsNumber(Number(quantity), "Quantity");
       return true;
     } catch (e) {
-      buyFormError.empty().append(e);
-      buyErrorMsg.removeClass("d-none");
+      buyFormError.text(e);
+      buyFormError.removeClass("d-none");
       return false;
     }
   });
 
-  //Add review AJAX call
+  //Review form submission
   reviewForm.submit(function (event) {
     event.preventDefault();
     try {
@@ -102,12 +100,13 @@
         })
         .fail(function (e) {
           errorMessage = JSON.parse(e.responseText);
-          reviewErrorMsg.empty().append(errorMessage.Error.message);
+          reviewErrorMsg.empty().append(errorMessage.error);
           reviewFormError.removeClass("d-none");
         });
     } catch (e) {
       //console.log(e);
-      reviewErrorMsg.empty().append(e);
+      //reviewErrorMsg.empty().append(e);
+      reviewFormError.text(e);
       reviewFormError.removeClass("d-none");
     }
   });
@@ -145,75 +144,16 @@
 
       $.ajax(requestConfig)
         .then(function (responseMessage) {
-          // var newHTML = `<div class="accordion-item">
-          //   <h4 class="accordion-header" id="heading_${responseMessage._id}">
-          //     <button
-          //       class="accordion-button"
-          //       type="button"
-          //       data-bs-toggle="collapse"
-          //       data-bs-target="#collapse_${responseMessage._id}"
-          //       aria-expanded="true"
-          //       aria-controls="collapse_${responseMessage._id}"
-          //     >
-          //     ${responseMessage.question} - User : ${responseMessage.questionBy}
-          //     </button>
-          //   </h4>
-          //   <div
-          //     id="collapse_${responseMessage._id}"
-          //     class="accordion-collapse collapse"
-          //     aria-labelledby="heading_${responseMessage._id}"
-          //     data-bs-parent="#accordionQ"
-          //   >
-          //     <div class="accordion-body" id="answerBody_${responseMessage._id}">
-          //       <h4>Answers</h4>
-          //       <div id="answerPanel_${responseMessage._id}">
-          //           <hr />
-          //           <div>No answers</div>
-          //           <hr />
-          //       </div>
-
-          //       <form class="answerForm" id="answerForm_${responseMessage._id}" method="POST" onclick="submitAnswer(event)">
-          //         <div class="mb-3">
-          //           <label for="answerText_${responseMessage._id}" class="form-label">Answer this
-          //             question</label>
-          //           <input
-          //             type="text"
-          //             name="answerText"
-          //             class="form-control"
-          //             id="answerText_${responseMessage._id}"
-          //           />
-          //           <input
-          //             type="hidden"
-          //             value="${responseMessage.questionBy}"
-          //             name="answerBy"
-          //             id="answerBy_${responseMessage._id}"
-          //           />
-          //           <input
-          //             type="hidden"
-          //             name="_method"
-          //             value="put"
-          //             id="method_${responseMessage._id}"
-          //           />
-          //           <input type="submit" value="Submit" />
-          //         </div>
-          //       </form>
-          //     </div>
-          //   </div>
-          // </div>`;
-          // $("#accordionQ").append(newHTML);
-          // $("#collapsePostQ").removeClass("show");
-          // $("#question").val("");
-
           window.location.reload();
         })
         .fail(function (e) {
           errorMessage = JSON.parse(e.responseText);
-          quesErrorMsg.empty().append(errorMessage.Error.message);
+          quesErrorMsg.empty().append(errorMessage.error);
           quesFormError.removeClass("d-none");
         });
     } catch (e) {
       //console.log(e);
-      quesErrorMsg.empty().append(e);
+      quesFormError.text(e);
       quesFormError.removeClass("d-none");
     }
   });
@@ -262,7 +202,6 @@
   // });
 
   $("select").change(function () {
-    //$(".alert").alert("close");
     var nameArr = $("#sneakerSize").val().split(",");
     console.log(nameArr[0]);
     if ($("#sneakerSize").val() == 0) {
@@ -282,13 +221,11 @@
 function submitAnswer(event) {
   event.preventDefault();
   var ansFormError;
-  var ansErrorMsg;
   try {
     var qID = event.currentTarget.id;
     qID = qID.replace("answerForm_", "");
 
     var ansFormError = $("#ansFormError_" + qID);
-    var ansErrorMsg = $("#ansErrorMsg_" + qID);
 
     ansFormError.addClass("d-none");
 
@@ -332,39 +269,56 @@ function submitAnswer(event) {
       })
       .fail(function (e) {
         errorMessage = JSON.parse(e.responseText);
-        ansErrorMsg.empty().append(errorMessage.Error.message);
+        ansFormError.text(errorMessage.error);
         ansFormError.removeClass("d-none");
       });
   } catch (e) {
-    ansErrorMsg.empty().append(e);
+    ansFormError.text(e);
     ansFormError.removeClass("d-none");
   }
 }
 
 function reportReview(event) {
-  let reportedBy = $("#reviewedBy").val();
-  let reportFor = event.currentTarget.id.split("_");
-  let reportR = $(`#rr_${reportFor[1]}`).val();
-  let request = {
-    method: "POST",
-    url: "/reports/",
-    dataType: "json",
-    data: {
-      reportedBy: reportedBy,
-      reportFor: reportFor[1],
-      reportReasons: reportR,
-      type: "Review",
-    },
-  };
-  $.ajax(request)
-    .then(function (responseMessage) {
-      alert("Review reported Successfully");
-      let idx = reportFor[1];
-      $(`#rrBtn_${idx}`).attr("hidden", true);
-    })
-    .fail(function (e) {
-      console.log(e);
-    });
+  try {
+    let reportedBy = $("#reviewedBy").val();
+    let reportFor = event.currentTarget.id.split("_");
+    let reportR = $(`#rr_${reportFor[1]}`).val();
+
+    var reportFormError = $("#rrFormError_" + reportFor[1]);
+
+    reportFormError.addClass("d-none");
+
+    checkInputStr(reportedBy, "Reported By");
+    checkInputStr(reportFor[1], "Report For");
+    checkInputStr(reportR, "Report Reason");
+
+    let request = {
+      method: "POST",
+      url: "/reports/",
+      dataType: "json",
+      data: {
+        reportedBy: reportedBy,
+        reportFor: reportFor[1],
+        reportReasons: reportR,
+        type: "Review",
+      },
+    };
+    $.ajax(request)
+      .then(function (responseMessage) {
+        alert("Review reported Successfully");
+        let idx = reportFor[1];
+        $(`#rrBtn_${idx}`).attr("hidden", true);
+      })
+      .fail(function (e) {
+        errorMessage = JSON.parse(e.responseText);
+        reportFormError.text(errorMessage.error);
+        reportFormError.removeClass("d-none");
+      });
+    $(`#rr_${reportFor[1]}`).val("");
+  } catch (e) {
+    reportFormError.text(e);
+    reportFormError.removeClass("d-none");
+  }
 }
 
 function openReport(event) {
@@ -382,27 +336,44 @@ function openReport2(event) {
 }
 
 function reportQna(event) {
-  let reportedBy = $("#reviewedBy").val();
-  let reportFor = event.currentTarget.id.split("_");
-  let reportR = $(`#rr_${reportFor[1]}`).val();
-  let request = {
-    method: "POST",
-    url: "/reports/",
-    dataType: "json",
-    data: {
-      reportedBy: reportedBy,
-      reportFor: reportFor[1],
-      reportReasons: reportR,
-      type: "QnA",
-    },
-  };
-  $.ajax(request)
-    .then(function (responseMessage) {
-      alert("Answer reported Successfully");
-      let idx = reportFor[1];
-      $(`#rqBtn_${idx}`).attr("hidden", true);
-    })
-    .fail(function (e) {
-      console.log(e);
-    });
+  try {
+    let reportedBy = $("#reviewedBy").val();
+    let reportFor = event.currentTarget.id.split("_");
+    let reportR = $(`#rq_${reportFor[1]}`).val();
+
+    var reportFormError = $("#rqFormError_" + reportFor[1]);
+
+    reportFormError.addClass("d-none");
+
+    checkInputStr(reportedBy, "Reported By");
+    checkInputStr(reportFor[1], "Report For");
+    checkInputStr(reportR, "Report Reason");
+
+    let request = {
+      method: "POST",
+      url: "/reports/",
+      dataType: "json",
+      data: {
+        reportedBy: reportedBy,
+        reportFor: reportFor[1],
+        reportReasons: reportR,
+        type: "QnA",
+      },
+    };
+    $.ajax(request)
+      .then(function (responseMessage) {
+        alert("Answer reported Successfully");
+        let idx = reportFor[1];
+        $(`#rqBtn_${idx}`).attr("hidden", true);
+      })
+      .fail(function (e) {
+        errorMessage = JSON.parse(e.responseText);
+        reportFormError.text(errorMessage.error);
+        reportFormError.removeClass("d-none");
+      });
+    $(`#rq_${reportFor[1]}`).val("");
+  } catch (e) {
+    reportFormError.text(e);
+    reportFormError.removeClass("d-none");
+  }
 }
